ff86a9e5adb8314551c1f2cf19090487793d25b9
wanjagi <franciswanjagi0@gmail.com>
2024-02-04 21:43:44 +0300

    change profile pic

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function myFunction() {
   var element = document.getElementById("body");
   element.classList.toggle("dark-mode");
 }
+
+const options = {
+  bottom: '560px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '1300px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: 'grey',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: 'Dark🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
